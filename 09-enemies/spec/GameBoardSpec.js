f078d416bcd5279fd702195f1366e4f676711ba8
adrianvinuelas <adrian.vinuelas@gmail.com>
2014-10-28 12:00:33 +0100

    carpeta modificada nuevamente

--- 09-enemies/spec/GameBoardSpec.js
+++ 09-enemies/spec/GameBoardSpec.js
@@ -56,3 +56,135 @@
     colisionado con objetos de cierto tipo, no con todos los objetos.
 
 */
+
+describe("Clase GameBoardSpec", function(){
+	
+	var gameBoard;
+	var canvas, ctx;
+	
+	beforeEach(function(){
+		loadFixtures('index.html');
+	
+		canvas = $('#game')[0];
+		expect(canvas).toExist();
+	
+		ctx = canvas.getContext('2d');
+		expect(ctx).toBeDefined();
+		
+		oldGame = Game;
+
+		gameBoard = new GameBoard();
+	});
+
+	afterEach(function(){
+        	Game = oldGame;
+   	});
+
+	it("ADD de gameboard", function(){
+		objeto1= {nave: "barco"};
+		gameBoard.add(objeto1);
+		expect(gameBoard.objects[0].nave).toEqual("barco");
+		
+		objeto2= "casa";
+		gameBoard.add(objeto2);
+		expect(gameBoard.objects[1]).toEqual(objeto2);
+
+		objeto3 = 4;
+		gameBoard.add(objeto3);
+		expect(gameBoard.objects[2]).toEqual(4);
+	});
+	
+	it("RESETREMOVED de gameboard", function(){
+		gameBoard.resetRemoved();
+		expect(gameBoard.removed.length).toEqual(0);
+	});
+
+	it("REMOVE de gameboard", function(){
+		objeto1 = 1;
+		objeto2 = 2;
+		gameBoard.add(objeto1);
+		gameBoard.add(objeto2);
+		gameBoard.resetRemoved();
+		expect(gameBoard.objects.length).toEqual(2);
+		expect(gameBoard.removed.length).toEqual(0);
+		gameBoard.remove(objeto2);
+		expect(gameBoard.removed.length).toEqual(1);
+	});
+
+	it("FINALIZEREMOVED de gameboard", function(){
+		objeto1 = 1;
+		objeto2 = 2;
+		gameBoard.add(objeto1);
+		gameBoard.add(objeto2);
+		gameBoard.resetRemoved();
+		gameBoard.remove(objeto1);
+		//gameBoard.remove(objeto2);
+		expect(gameBoard.removed.length).toEqual(1);
+		gameBoard.finalizeRemoved();
+		expect(gameBoard.objects.length).toEqual(1);
+	});
+
+	it("ITERATE de gameBoard", function(){
+		spyOn(ctx, "drawImage");
+		Game = {width: 320, height: 480};
+		SpriteSheet.load (sprites,function(){});
+		var miNave = new PlayerShip();
+		gameBoard.add(miNave);
+		gameBoard.iterate('draw',ctx);
+		expect(ctx.drawImage.calls[0].args[3]).toEqual(37);
+		expect(ctx.drawImage.calls[0].args[4]).toEqual(42);
+	});
+
+	it("DRAW de gameboard", function(){
+		spyOn(ctx, "fillText");
+		var titulo = "titulo";
+		var subtitulo = "subtitulo";
+		var ts1 = new TitleScreen(titulo, subtitulo);
+		gameBoard.add(ts1);
+		gameBoard.draw(ctx);
+		expect(ctx.fillText.calls[0].args[0]).toEqual(titulo);
+  		expect(ctx.fillText.calls[1].args[0]).toEqual(subtitulo);
+	});
+
+	it("STEP de gameboard", function(){
+		SpriteSheet.load (sprites,function(){});
+		// Hacemos que se pulse la tecla left:
+		Game = {width: 320, height: 480, keys: {'left': true}};
+		// Creamos un PlayerShip para testar
+		var miNave = new PlayerShip();
+		gameBoard.add(miNave);
+		gameBoard.step(0.5);// Hacemos como que ha pasado 0.5s
+		expect(gameBoard.objects[0].x).toEqual(41.5);
+	});
+	
+	it("OVERLAP de gameboard", function(){
+		SpriteSheet.load (sprites,function(){});
+		Game = {width: 320, height: 480, keys: {'left': true}};
+		var miNave = new PlayerShip();
+		var miNave2 = new PlayerShip();
+		expect(gameBoard.overlap(miNave,miNave2)).toBe(true);
+		miNave.step(0.5)
+		expect(gameBoard.overlap(miNave,miNave2)).toBe(false);
+	});
+
+	it("OVERLAP y COLLIDE de gameboard", function(){
+		SpriteSheet.load (sprites,function(){});
+		Game = {width: 320, height: 480, keys: {'left': true}};
+		var miNave = new PlayerShip();
+		var miNave2 = new PlayerShip();
+		miNave2.step(1)
+		gameBoard.add(miNave);
+		gameBoard.add(miNave2);
+		expect(gameBoard.collide(miNave)).toBe(false);
+	});
+
+	it("DETECT de gameboard", function(){
+		var obj = {}
+		funcion = function(obj){
+			return true;
+		}
+		gameBoard.add(obj);
+		expect(gameBoard.detect(funcion)).toBe(obj);
+	});
+
+});
--- 09-enemies/spec/PlayerMissileSpec.js
+++ 09-enemies/spec/PlayerMissileSpec.js
@@ -27,3 +27,78 @@
     la clase en el prototipo
 
 */
+
+describe("pruebas de PlayerMissileSpec", function(){
+
+	var canvas, ctx;
+	
+	beforeEach(function(){
+		loadFixtures('index.html');
+	
+		canvas = $('#game')[0];
+		expect(canvas).toExist();
+	
+		ctx = canvas.getContext('2d');
+		expect(ctx).toBeDefined();
+		
+		oldGame = Game;
+
+	});
+
+	afterEach(function(){
+        	Game = oldGame;
+   	});
+
+	it("prueba de step",function(){
+		var objBoard = { remove: function(obj) {} }; //tablero imaginario que necesitamos crear para hacer la prueba 
+		spyOn(objBoard, "remove");
+		
+		var gameMissile = new PlayerMissile(10,71);
+		gameMissile.board = objBoard;
+		
+		expect(gameMissile.x).toEqual(9);
+		expect(gameMissile.y).toEqual(61);
+
+		var dt = 1;
+   		gameMissile.step(dt);
+
+		expect(gameMissile.board.remove).toHaveBeenCalled();
+   		expect(gameMissile.y).toEqual(-639);
+		
+		var gameMissile2 = new PlayerMissile(47,61);
+		gameMissile2.board = objBoard;
+		expect(gameMissile2.x).toEqual(46);
+		expect(gameMissile2.y).toEqual(51);
+		
+		dt= 2;
+		gameMissile2.step(dt);
+
+		expect(gameMissile2.board.remove).toHaveBeenCalled();
+   		expect(gameMissile2.y).toEqual(-1349);
+	});
+
+	it("prueba de draw", function(){
+		SpriteSheet.load (sprites,function(){});
+		spyOn(ctx, "drawImage");
+		
+		var gameMissile = new PlayerMissile(10,71);
+		gameMissile.draw(ctx);
+		
+		
+		expect(ctx.drawImage.calls[0].args[1]).toEqual(0);
+		expect(ctx.drawImage.calls[0].args[2]).toEqual(30);
+		expect(ctx.drawImage.calls[0].args[3]).toEqual(2);
+		expect(ctx.drawImage.calls[0].args[4]).toEqual(10);
+
+		
+		objSprite = { draw: function(ctx,name,x,y) {} };
+		var gameMissile2 = new PlayerMissile(10,71);
+		SpriteSheet = objSprite;
+		spyOn(SpriteSheet, "draw");
+		gameMissile2.draw(ctx);
+
+		expect(SpriteSheet.draw).toHaveBeenCalled();
+		
+		
+	});
+});
